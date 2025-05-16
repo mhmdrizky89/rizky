0150128ab53f90baf65a3c9061e58d160ad67ceb
MhmdRizky <mhmdrizky05707@gmail.com>
2025-05-16 07:59:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// Greeting berdasarkan waktu
+const greeting = document.getElementById("greeting");
+const jam = new Date().getHours();
 
+if (jam >= 5 && jam < 12) {
+  greeting.textContent = "Selamat Pagi, Selamat Datang di Dunia Digital";
+} else if (jam >= 12 && jam < 17) {
+  greeting.textContent = "Selamat Siang, Selamat Datang di
