ed03fd452be1288fd8bf122f03390341134af58a
MhmdRizky <mhmdrizky05707@gmail.com>
2025-05-16 09:20:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const wrapper = document.querySelector(".wrapper");
 const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Aaaaa, I like you too";
+  question.innerHTML = "Aaaaa, aku juga suka kamu!";
   gif.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
 
